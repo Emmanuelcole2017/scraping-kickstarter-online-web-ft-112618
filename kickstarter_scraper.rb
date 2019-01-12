24a5218b7c024b0077805e4fe963610056a7ea96
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-01-12 09:38:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,6 +12,9 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  projects = {}
+  
+  
   binding.pry
 end
 
